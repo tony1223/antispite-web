0446b65b26ce1b7fa34ceaa432394da7d2ac7dd6
TonyQ <tonylovejava@gmail.com>
2014-04-19 17:59:59 +0800

    finetune

--- application/helpers/comment_helper.php
+++ application/helpers/comment_helper.php
@@ -3,7 +3,7 @@ function comment_user_link($comment){
 	
 	if($comment["type"] =="FBComment"){
 		if(strpos($comment["userkey"],"id=") !== False){
-			return "https://www.facebook.com/profile.php".$comment["userkey"];
+			return "https://www.facebook.com/profile.php?".$comment["userkey"];
 		}else{
 			return "https://www.facebook.com/".$comment["userkey"];
 		}
