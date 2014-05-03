68550b0986f771b7b6fafc0a56b851bdb802c9a2
fripig <fripig@gmail.com>
2014-05-03 21:50:04 +0800

    用flot.js畫圖表

--- application/views/comment/user.php
+++ application/views/comment/user.php
@@ -21,7 +21,15 @@
 	
 	<h2><?=$comments[0]["name"] ?> 近期跳針留言清單（<?=count($comments)>=100 ?"最近一百筆" :count($comments) ?>）<div style="float:right;" class="fb-like" data-href="http://antispite.tonyq.org/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div></h2>
 	<p>跳針指數：<?=$count?></p>
-	<div id="chart_div" style="width: 100%; height: 500px;"></div>
+	<div id="chart_div" style="width: 100%; height: 500px;">
+		<div id="placeholder" class="demo-placeholder" style="float:left; width:650px;height:800px;"></div>
+		<div id="overview" class="demo-placeholder" style="float:right;width:260px; height:225px;"></div>
+		<div id="info">
+		    time:<p id="item_time"></p>
+		    time:<p id="item_count"></p>
+		    time:<p id="item_content"></p>
+		</div>
+	</div>
 	<hr />
 	<table class="table table-bordered table-comment">
 		<tr class="comment-row">
@@ -119,26 +127,121 @@ foreach($comments as $comment){
 ?>
 <script>
 	window.datas = <?=json_encode($data)?>;
-	for(var i =0 ; i < window.datas.length;++i){
-		window.datas[i][0] = new Date(window.datas[i][0]);
-	}
+
 </script>
 <?php function js_section(){ ?>
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript" src="http://www.flotcharts.org/flot/jquery.flot.js"></script>
+    <script type="text/javascript" src="http://www.flotcharts.org/flot/jquery.flot.time.js"></script>
+    <script type="text/javascript" src="http://www.flotcharts.org/flot/jquery.flot.selection.js"></script>
     <script type="text/javascript">
-      google.load('visualization', '1', {'packages':['annotatedtimeline']});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('datetime', '時間');
-        data.addColumn('number', '字數');
-        data.addColumn('string', '內容');
-        
-        data.addRows(window.datas);
+ 	$(function() {
 
-        var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('chart_div'));
-        chart.draw(data, {displayAnnotations: true});
-      }
+		// setup plot
+
+		function getData(x1, x2) {
+            var data = window.datas;
+			var d = [];
+            datalength = data.length-1;
+			for (var i = 0; i <= datalength; ++i) {
+               //console.log(data[i]);
+				if(data[i][0]>=x1 && data[i][0]<=x2)
+				    d.push(data[i]);
+			}
+
+			return [
+				{ label: "filter data", data: d }
+			];
+		}
+
+		var options = {
+            grid:{
+            hoverable:true
+            },
+			legend: {
+				show: false
+			},
+			series: {
+				points: {
+					show: true,
+                    radius:5
+				}
+			},
+
+            xaxis: { mode: "time" },
+			selection: {
+				mode: "xy"
+			}
+		};
+
+		var startData = getData(0, 3 * Math.PI);
+
+		var plot = $.plot("#placeholder", [window.datas], options);
+
+		// Create the overview plot
+
+		var overview = $.plot("#overview", [window.datas], {
+			legend: {
+				show: false
+			},
+			series: {
+                points: {
+					show: true
+				},
+				shadowSize: 0
+			},
+			xaxis: {
+				 mode: "time" 
+			},
+
+			grid: {
+				color: "#999"
+			},
+			selection: {
+				mode: "xy"
+			}
+		});
+
+		// now connect the two
+
+		$("#placeholder").bind("plotselected", function (event, ranges) {
+
+			// clamp the zooming to prevent eternal zoom
+			if (ranges.xaxis.to - ranges.xaxis.from < 0.00001) {
+				ranges.xaxis.to = ranges.xaxis.from + 0.00001;
+			}
+			if (ranges.yaxis.to - ranges.yaxis.from < 0.00001) {
+				ranges.yaxis.to = ranges.yaxis.from + 0.00001;
+			}
+
+			// do the zooming
+
+			plot = $.plot("#placeholder", getData(ranges.xaxis.from, ranges.xaxis.to),
+				$.extend(true, {}, options, {
+					xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to },
+					yaxis: { min: ranges.yaxis.from, max: ranges.yaxis.to }
+				})
+			);
+
+			// don't fire event on the overview to prevent eternal loop
+
+			overview.setSelection(ranges, true);
+		});
+
+		$("#overview").bind("plotselected", function (event, ranges) {
+			plot.setSelection(ranges);
+		});
+        $("#placeholder").bind("plothover", function (event, pos, item) {
+            if(item)
+            {
+			row = item.series.data[item.dataIndex];
+                $("#item_time").text(new Date(row[0]));
+                $("#item_count").text(row[1]);
+                $("#item_content").text(row[2]);
+            }
+		});
+
+
+	});
     </script>
 
 <?php if(is_login()){?>
