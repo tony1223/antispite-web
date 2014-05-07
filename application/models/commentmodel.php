2f4f50b9889908753b2988abf211a729b3dfc15f
TonyQ <tonylovejava@gmail.com>
2014-05-08 07:52:48 +0800

    finetune

--- application/models/commentmodel.php
+++ application/models/commentmodel.php
@@ -231,8 +231,14 @@ class CommentModel extends MONGO_MODEL {
 				->where("status",CommentModel::STATUS_BAD)->limit(200)->get($this->_collection);
 	}
 	
-	public function get_all_by_user($key){
-		return $this->mongo_db->orderBy("time","desc")->where("userkey",$key)->limit(500)->get($this->_collection);
+	public function get_all_by_user($key,$status = null){
+		
+		if($status =="" || $status == null){
+			return $this->mongo_db->orderBy("time","desc")->where("userkey",$key)->where("status".intval($status,10))->limit(500)->get($this->_collection);
+		}else{
+			return $this->mongo_db->orderBy("time","desc")->where("userkey",$key)->limit(500)->get($this->_collection);
+		}
+		
 	}
 	
 	public function get_bads_by_url($key){
